d402d349b3208b023a33f729c9acc6f9f7610deb
David Kleiven <davidkleiven446@gmail.com>
2020-09-30 10:41:44 +0200

    BUG: Calculate bias properly

--- gafit/fic.go
+++ gafit/fic.go
@@ -14,20 +14,22 @@ type PredictionErrorFIC struct {
 
 // Evaluate evaluates the focused information criteria
 func (pef *PredictionErrorFIC) Evaluate(X *mat.Dense, y *mat.VecDense, coeff *mat.VecDense) float64 {
-	r, c := X.Dims()
 	rss := Rss(X, y, coeff)
+	pred := Pred(X, coeff)
 	cov, err := CovMatrix(X, rss)
 	if err != nil {
 		return math.Inf(1)
 	}
 
 	variancePredError := 0.0
+	biasSq := 0.0
 	res := mat.NewDense(1, 1, nil)
 	for _, c := range pef.Data {
 		res.Product(X.RowView(c).T(), cov, X.RowView(c))
 		variancePredError += res.At(0, 0)
+		biasSq += math.Pow(y.AtVec(c)-pred.AtVec(c), 2.0)
 	}
 
-	totalVariance := rss/float64(r-c) + variancePredError
+	totalVariance := biasSq + variancePredError
 	return math.Sqrt(totalVariance)
 }
